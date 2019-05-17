ebdf2060c8f9e0c452a415914a7402b03b8c3885
idid <loveyourmountain@gmail.com>
2019-05-17 16:38:53 +0300

    two methods one class

---
+++ src/Main.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double deposit = 0;
+        double annualRate = 0;
+        double factor = 0;
+
+        while (deposit == 0) {
+            deposit = setNum("deposit");
+        }
+        while (annualRate == 0) {
+            annualRate = setNum("annual rate");
+        }
+        while (factor == 0) {
+            factor = setNum(deposit);
+        }
+        System.out.println("deposit: " + deposit + " annual rate: " + annualRate + " factor: " + factor);
+        System.out.println("required sum: " + deposit * factor);
+        List nums = calc(deposit, annualRate, factor);
+        System.out.print("your sum: ");
+        System.out.format("%,.2f%n", nums.get(1));
+        System.out.println("years: " + nums.get(0));
+    }
+
+    private static double setNum(String type) {
+        double num;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("input " + type + ": ");
+        if (scanner.hasNextDouble() && (num = scanner.nextDouble()) > 0) {
+            return num;
+        } else {
+            System.out.println("please insert positive numeric type");
+            return 0;
+        }
+    }
+
+    private static double setNum(double deposit) {
+        double num = 0;
+        while (num == 0) {
+            num = setNum("factor");
+            if ((num * deposit) >= deposit) {
+                return num;
+            } else {
+                System.out.println("factor should be more than 1");
+                num = 0;
+            }
+        }
+        return num;
+    }
+
+    private static List calc(double deposit, double annualRate, double factor) {
+        List<Number> numbers = new ArrayList<>();
+        int year = 0;
+        double total = deposit * factor;
+        while (deposit <= total) {
+            deposit = deposit + (deposit * annualRate) / 100;
+            year++;
+        }
+        numbers.add(year);
+        numbers.add(deposit);
+        return numbers;
+    }
+
+}
+
+
+
